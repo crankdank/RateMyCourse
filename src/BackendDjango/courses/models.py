1f70db005163d8f90df8baafa39a71c0181cc022
ZjTan4 <ZTAN4@UALBERTA.CA>
2022-01-15 13:49:24 -0800

    Course model halfway

--- src/BackendDjango/courses/models.py
+++ src/BackendDjango/courses/models.py
@@ -1,3 +1,16 @@
+from pydoc_data.topics import topics
+from pyexpat import model
+from statistics import mode
+from turtle import title
 from django.db import models
+import uuid
 
 # Create your models here.
+class Course(models.Model):
+    # field in db
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    
+    prefix = models.CharField(max_length=10, default="CMPUT")
+    courseCode = models.CharField(max_length=5)
+    title = models.CharField(max_length=50)
+    topics = models.CharField(max_length=200)
