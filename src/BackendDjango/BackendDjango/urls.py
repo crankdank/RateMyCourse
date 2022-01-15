5c3cb844a3c6beba7cf044d64f6e21d57562aca0
Bingshen Yang <bi2@ualberta.ca>
2022-01-15 15:05:16 -0700

    review part model done

--- src/BackendDjango/BackendDjango/urls.py
+++ src/BackendDjango/BackendDjango/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from reviews import reviews_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('reviews/', reviews_views.all_reviews, name="all_reviews"),
 ]
