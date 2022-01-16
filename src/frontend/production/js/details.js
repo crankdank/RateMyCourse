30dac25b818a3f8f4bd03e9ae0143d07c9a08263
Rudy de Lorenzo <rdelorenzo5@gmail.com>
2022-01-16 11:37:36 -0700

    Updated frontend with generic URLs
    Signed-off-by: Rudy de Lorenzo <rdelorenzo5@gmail.com>

--- src/frontend/production/js/details.js
+++ src/frontend/production/js/details.js
@@ -1,62 +1,66 @@
-const params = new URLSearchParams((new URL(window.location.href)).search);
-console.log(params.get("subj"));
-console.log(params.get("num"));
+async function populate() {
+  const params = new URLSearchParams((new URL(window.location.href)).search);
 
-const subject = "CMPUT";
-const courseNumber = 401
+  const subject = params.get("subj");
+  const courseNumber = params.get("num");
 
-const courseInfo = {
-  "faculty": "Science",
-  "subject": "CMPUT",
-  "course_num": "401",
-  "average_overall": 4.5,
-  "average_workload": 3.1,
-  "average_interest": 4.5,
-  "average_usefulness": 5.0,
-  "average_difficulty": 2.0,
-  "assignedProfs": "TBA",
-  "hasLab": false
+  let courseInfo = {
+    "faculty": "Science",
+    "subject": "CMPUT",
+    "course_num": "401",
+    "average_overall": 4.5,
+    "average_workload": 3.1,
+    "average_interest": 4.5,
+    "average_usefulness": 5.0,
+    "average_difficulty": 2.0,
+    "assignedProfs": "TBA",
+    "hasLab": false
+  }
+
+  //courseInfo = (await (await fetch(`http://localhost:8000/subject/${subject}/number/${courseNumber}`)).json())[0];
+
+  const data = {
+    datasets: [{
+      data: [courseInfo.average_overall, 5 - courseInfo.average_overall],
+      backgroundColor: [
+        'rgb(39, 93, 56)',
+        'rgba(255, 255, 255, 0)'
+      ],
+      hoverOffset: 4
+    }]
+  };
+
+  const config = {
+    type: 'doughnut',
+    data: data,
+    options: {
+      plugins: {
+        tooltip: {
+          enabled: false
+        }
+      },
+      events: [],
+      cutout: "65%"
+    }
+  };
+
+  const myChart = new Chart(
+    document.getElementById('doughnutChart'),
+    config
+  );
+
+  document.querySelector("p.back").addEventListener("click", (e) => {
+    window.open("/", "_self");
+  })
+
+  // Set all course data
+  document.querySelector("#title").innerHTML = `${courseInfo.subject} ${courseInfo.course_num}`;
+  document.querySelector("#fac").innerHTML = `Faculty of ${courseInfo.faculty}`;
+  document.querySelector("#overallRating").innerHTML = `${courseInfo.average_overall.toFixed(1)}`;
+  document.querySelector("#diff").innerHTML = `${courseInfo.average_difficulty.toFixed(1)}`;
+  document.querySelector("#use").innerHTML = `${courseInfo.average_usefulness.toFixed(1)}`;
+  document.querySelector("#work").innerHTML = `${courseInfo.average_workload.toFixed(1)}`;
+  document.querySelector("#interest").innerHTML = `${courseInfo.average_interest.toFixed(1)}`;
 }
 
-const data = {
-  datasets: [{
-    data: [courseInfo.average_overall, 5-courseInfo.average_overall],
-    backgroundColor: [
-      'rgb(39, 93, 56)',
-      'rgba(255, 255, 255, 0)'
-    ],
-    hoverOffset: 4
-  }]
-};
-
-const config = {
-  type: 'doughnut',
-  data: data,
-  options: {
-    plugins: {
-      tooltip: {
-        enabled: false
-      }
-    },
-    events: [],
-    cutout: "65%"
-  }
-};
-
-const myChart = new Chart(
-  document.getElementById('doughnutChart'),
-  config
-);
-
-document.querySelector("p.back").addEventListener("click", (e) => {
-  window.open("/", "_self");
-})
-
-// Set all course data
-document.querySelector("#title").innerHTML = `${courseInfo.subject} ${courseInfo.course_num}`;
-document.querySelector("#fac").innerHTML = `Faculty of ${courseInfo.faculty}`;
-document.querySelector("#overallRating").innerHTML = `${courseInfo.average_overall.toFixed(1)}`;
-document.querySelector("#diff").innerHTML = `${courseInfo.average_difficulty.toFixed(1)}`;
-document.querySelector("#use").innerHTML = `${courseInfo.average_usefulness.toFixed(1)}`;
-document.querySelector("#work").innerHTML = `${courseInfo.average_workload.toFixed(1)}`;
-document.querySelector("#interest").innerHTML = `${courseInfo.average_interest.toFixed(1)}`;
+populate();
--- src/frontend/production/js/main.js
+++ src/frontend/production/js/main.js
@@ -4,14 +4,32 @@ function getValueOrPlaceholderSelector(selector) {
   return el.value === "" ? el.placeholder : el.value;
 }
 
-document.querySelector("p.search").addEventListener("click", (e) => {
+document.querySelector("p.search").addEventListener("click", async (e) => {
   let subject = getValueOrPlaceholderSelector("#csub").toUpperCase();
   let number = getValueOrPlaceholderSelector("#cnum").toUpperCase();
 
   // CHECK THAT COURSE EXISTS AND THAT BOTH BOXES HAVE A VALUE
+  try {
+    let ret = (await (await fetch(`http://localhost:8000/subject/${subject}/number/${number}`)).json());
+    if (ret.length !== 1) {
+      swal({
+        title: "Error!",
+        text: "We couldn't find that course! Make sure you filled in both fields correctly.",
+        icon: "error",
+      });
+      return
+    }
 
-  let para = new URLSearchParams();
-  para.append("subj", subject);
-  para.append("num", number);
-  window.open("/details.html?" + para.toString(), "_self");
+    let para = new URLSearchParams();
+    para.append("subj", subject);
+    para.append("num", number);
+    window.open("/details.html?" + para.toString(), "_self");
+  } catch (e) {
+    swal({
+      title: "Error!",
+      text: "Couldn't communicate with the server.",
+      icon: "error",
+    });
+  }
+
 });
