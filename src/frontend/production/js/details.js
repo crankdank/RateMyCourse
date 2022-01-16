930059b211c5da5aa42eec5ddc4c9ba62c5fe049
Rudy de Lorenzo <rdelorenzo5@gmail.com>
2022-01-15 23:47:22 -0700

    Quick look details done
    Signed-off-by: Rudy de Lorenzo <rdelorenzo5@gmail.com>

--- src/frontend/production/js/details.js
+++ src/frontend/production/js/details.js
@@ -1,1 +1,31 @@
 let courseID = 1234;
+
+const data = {
+  datasets: [{
+    data: [4.5, 0.5],
+    backgroundColor: [
+      'rgb(39, 93, 56)',
+      'rgba(255, 255, 255, 0)'
+    ],
+    hoverOffset: 4
+  }]
+};
+
+const config = {
+  type: 'doughnut',
+  data: data,
+  options: {
+    plugins: {
+      tooltip: {
+        enabled: false
+      }
+    },
+    events: [],
+    cutout: "70%"
+  }
+};
+
+const myChart = new Chart(
+  document.getElementById('doughnutChart'),
+  config
+);
