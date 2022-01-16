7af446230dfbc9273f17547d7aa7d9007eec4087
Rudy de Lorenzo <rdelorenzo5@gmail.com>
2022-01-16 10:22:32 -0700

    Website compiles with Parcel
    Signed-off-by: Rudy de Lorenzo <rdelorenzo5@gmail.com>

---
+++ src/frontend/production/dist/index.7f943bb2.js
@@ -0,0 +1,14 @@
+function getValueOrPlaceholderSelector(selector) {
+    let el = document.querySelector(selector);
+    return el.value === "" ? el.placeholder : el.value;
+}
+document.querySelector("p.search").addEventListener("click", (e)=>{
+    let subject = getValueOrPlaceholderSelector("#csub").toUpperCase();
+    let number = getValueOrPlaceholderSelector("#cnum").toUpperCase(); // CHECK THAT COURSE EXISTS AND THAT BOTH BOXES HAVE A VALUE
+    let para = new URLSearchParams();
+    para.append("subj", subject);
+    para.append("num", number);
+    window.open("/details.html?" + para.toString(), "_self");
+});
+
+//# sourceMappingURL=index.7f943bb2.js.map
