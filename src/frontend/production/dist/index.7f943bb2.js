59c229dff557d75eee6cb736586df5e94cf008af
Rudy de Lorenzo <rdelorenzo5@gmail.com>
2022-01-16 10:09:48 -0700

    Frontend transpiled and ready for production
    Signed-off-by: Rudy de Lorenzo <rdelorenzo5@gmail.com>

--- src/frontend/production/dist/index.7f943bb2.js
+++
@@ -1,7 +0,0 @@
-document.querySelector("p.search").addEventListener("click", (e)=>{
-    let para = new URLSearchParams();
-    para.append("courseID", "VALUE");
-    window.open("/details.html?" + para.toString(), "_self");
-});
-
-//# sourceMappingURL=index.7f943bb2.js.map
--- src/frontend/production/dist/index.b79ca670.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("p.search").addEventListener("click",(e=>{let t=new URLSearchParams;t.append("courseID","VALUE"),window.open("/details.html?"+t.toString(),"_self")}));
-//# sourceMappingURL=index.b79ca670.js.map
