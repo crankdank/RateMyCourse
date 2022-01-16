b57808b8fbe72f3852d8074f44374f7188f9ee4c
Rudy de Lorenzo <rdelorenzo5@gmail.com>
2022-01-16 03:04:51 -0700

    Completed REACT reviews
    Signed-off-by: Rudy de Lorenzo <rdelorenzo5@gmail.com>

--- src/frontend/production/js/reactReviews.js
+++ src/frontend/production/js/reactReviews.js
@@ -1,23 +1,68 @@
 function Review(props) {
+  return (
+    <div className="review">
+      <div className="metrics">
+        <div className="rating">
+          <p className="big-digit enlarge">{props.data.overall_rate}</p>
+          <p className="small-digit">/5</p>
+        </div>
+        <div className="review-table">
+          <p className="category minify">DIFFICULTY</p>
+          <p className="big-digit minify">{props.data.difficulty}</p>
 
+          <p className="category minify">USEFULNESS</p>
+          <p className="big-digit minify">{props.data.usefulness}</p>
+
+          <p className="category minify">WORKLOAD</p>
+          <p className="big-digit minify">{props.data.workload}</p>
+
+          <p className="category minify">INTEREST</p>
+          <p className="big-digit minify">{props.data.interest}</p>
+        </div>
+      </div>
+      <div className="details">
+        <div className="section">
+          <div className="consecutive-container">
+            <p className="sec-title">PROFESSOR</p>
+            <p className="sec-text">{props.data.professor}</p>
+          </div>
+          <div className="consecutive-container">
+            <p className="sec-title">SECTION</p>
+            <p className="sec-text">{props.data.semester}</p>
+          </div>
+        </div>
+        <p className="review-text">{props.data.comment}</p>
+        <div className="review-metadata">
+          <p className="author">Submitted by delorenz</p>
+          <p className="posted-date">{props.data.date}</p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 function ReviewsList() {
   const reviews = [{
-    "professor": "Dr.T",
-    "semester": "WINTER",
-    "course_num": "CMPUT174",
-    "overall_rate": 1,
-    "difficulty": 1,
-    "usefulness": 1,
-    "workload": 1,
-    "interest": 1,
-    "comment": "123",
-    "date": "2022-01-16"
+    "professor": "Dr. Imanuelov",
+    "semester": "WINTER22",
+    "course_num": "401",
+    "overall_rate": 4,
+    "difficulty": 2,
+    "usefulness": 5,
+    "workload": 3,
+    "interest": 4,
+    "comment": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam amet assumenda" +
+      " cumque dolorum est facere, illum, inventore ipsam itaque mollitia," +
+      " officiis omnis quas qui quisquam saepe sed unde voluptate voluptatem. Lorem ipsum dolor sit amet," +
+      " consectetur adipisicing elit. Aperiam consequatur consequuntur cum dignissimos eum eveniet facere in" +
+      " incidunt labore natus, provident quam qui quisquam reiciendis" +
+      " repellendus repudiandae rerum similique sunt voluptas. Lorem ipsum dolor sit amet, consectetur" +
+      " adipisicing elit. A aliquam corporis eaque ex fuga ipsam nihil ratione sint ut voluptatem?",
+    "date": "16-01-2022"
   },{
     "professor": "Dr.H",
     "semester": "FALL22",
-    "course_num": "CMPUT174",
+    "course_num": "401",
     "overall_rate": 5,
     "difficulty": 3,
     "usefulness": 2,
@@ -30,7 +75,7 @@ function ReviewsList() {
   },{
     "professor": "Dr.R",
     "semester": "SUMMER15",
-    "course_num": "CMPUT174",
+    "course_num": "401",
     "overall_rate": 3,
     "difficulty": 1,
     "usefulness": 0,
@@ -43,7 +88,8 @@ function ReviewsList() {
   }];
   return (
     <div className="reviews">
-      {reviews.map((reviewData) => <Review data={reviewData}/>)};
+      <h1 className="reviews-header">REVIEWS</h1>
+      {reviews.map((reviewData) => <Review data={reviewData}/>)}
     </div>
   );
 }
